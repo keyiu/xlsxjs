5845cedcbae707fd1a453cafcc4108678c1cd2a7
吴伟杰 <wu.weijie@qingniu.co>
2017-10-18 11:19:52 +0800

    提交一下

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,21 @@ module.exports = {
   "parserOptions": {
     "ecmaVersion": 6,
   },
+  "rules": {
+    "no-undef": 2,
+    "no-invalid-this": 0,
+    "curly": 2,
+    "max-len": [2, 120, 4],
+  },
+  "globals": {
+    "process": true,
+    "require": true,
+    "module": true,
+    "__dirname": true,
+    "Promise": true,
+    Map: true,
+    describe: true,
+    it: true,
+  },
   "extends": "google"
 };
